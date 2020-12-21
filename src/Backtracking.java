ed0a88904fe7adfcb2d5a6f2f0b1aad1e697acff
Reza ADB <adbrezaooo@gmail.com>
2020-12-21 16:46:12 +0330

    Backtracking: GetCandidateValuesForVariable method and CandidateValue class added.

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -1,29 +1,41 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Random;
 
 public class Backtracking {
     int variablesCount;
+    Random randomGenerator;
 
-    public Backtracking(int variablesCount) {
+    public Backtracking(int variablesCount, Random randomGenerator) {
         this.variablesCount = variablesCount;
+        this.randomGenerator = randomGenerator;
     }
 
+    /**
+     * run recursive backtracking algorithm to fill all variables using N_Queen constraints.
+     *
+     * @param assignments pairs of variables and their values.
+     * @return "true" if we found an result, otherwise "false".
+     */
     public boolean FindResult(HashMap<Integer, Integer> assignments) {
+        System.out.println(assignments.size());
         if (assignments.size() == variablesCount)
             return true;
         HashSet<Integer> conflictedValues = new HashSet<>();
         int variable = FindVariableWithMRV(assignments, conflictedValues);
-        for (int value = 0; conflictedValues.size() < variablesCount && value < variablesCount; value++)
-            if (!conflictedValues.contains(value)) {
-                assignments.put(variable, value);
-                if (FindResult(assignments))
-                    return true;
-                else {
-                    assignments.remove(variable);
-                }
-            }
-        if (assignments.size() > 220)
-            System.out.println(assignments.size());
+        if (conflictedValues.size() == variablesCount)
+            return false;
+        PriorityQueue<CandidateValue> candidateValues = GetCandidateValuesForVariable(assignments, variable, conflictedValues);
+        while (!candidateValues.isEmpty()) {
+            int value = candidateValues.poll().value;
+//            if (!conflictedValues.contains(value)) {
+            assignments.put(variable, value);
+            if (FindResult(assignments))
+                return true;
+            assignments.remove(variable);
+//            }
+        }
         return false;
     }
 
@@ -36,7 +48,7 @@ public class Backtracking {
      */
     private int FindVariableWithMRV(HashMap<Integer, Integer> assignments, HashSet<Integer> conflictedValues) {
         if (assignments.isEmpty()) {
-            return variablesCount / 2;
+            return randomGenerator.nextInt(variablesCount);
         }
         int maxConflictedValuesCount = 0;
         int bestVariable = -1;
@@ -64,4 +76,148 @@ public class Backtracking {
         return bestVariable;
     }
 
+    /**
+     * get possible values and their conflicts for given variable using assignments( some kind of map for those variables that we set a value for them ).
+     *
+     * @param assignments      hash map of variables( as keys ) and values ( well ... as values ) that we already fount( it means we set those pairs before ).
+     * @param variable         index of the variable that we want find some values for.
+     * @param conflictedValues values that are out of the variable's domain of values.
+     * @return priority queue of condition values( order is number of conflicts. ).
+     */
+    public PriorityQueue<CandidateValue> GetCandidateValuesForVariable(HashMap<Integer, Integer> assignments, int variable, HashSet<Integer> conflictedValues) {
+        PriorityQueue<CandidateValue> candidateValues = new PriorityQueue<>();
+        for (int value = 0; value < variablesCount; value++) {
+            if (!conflictedValues.contains(value)) {
+                HashMap<Integer, HashSet<Integer>> intersections = new HashMap<>();
+                int conflicts = -3;
+                /**
+                 * find points that are already out of other variables domains because of a pair of variable-value
+                 * so those values are out of this pair's new conflicts( it means this pair doesn't remove those values from a variable domain ).
+                 */
+                for (int anotherVariable : assignments.keySet()) {
+                    /**
+                     * a: line1, b: line2, c: intersection point.
+                     * a   b
+                     *   c
+                     * b   a
+                     */
+                    int m1 = 1;
+                    int m2 = -1;
+                    int b1 = variable - (m1 * value);
+                    int b2 = anotherVariable - (m2 * assignments.get(anotherVariable));
+                    float intersectionValue = (float) (b1 - b2) / (float) (m2 - m1);
+                    if (intersectionValue % 1 == 0 && intersectionValue >= 0 && intersectionValue < variablesCount) {
+                        int intersectionVariable = b2 - m2 * (int) intersectionValue;
+                        if (!intersections.containsKey(intersectionVariable))
+                            intersections.put(intersectionVariable, new HashSet<>());
+                        intersections.get(intersectionVariable).add((int) intersectionValue);
+                    }
+                    /**
+                     * a: line1, b: line2, c: intersection point.
+                     * b   a
+                     *   c
+                     * a   b
+                     */
+                    m1 = -1;
+                    m2 = 1;
+                    b1 = variable - (m1 * value);
+                    b2 = anotherVariable - (m2 * assignments.get(anotherVariable));
+                    intersectionValue = (float) (b1 - b2) / (float) (m2 - m1);
+                    if (intersectionValue % 1 == 0 && intersectionValue >= 0 && intersectionValue < variablesCount) {
+                        int intersectionVariable = b2 - m2 * (int) intersectionValue;
+                        if (!intersections.containsKey(intersectionVariable))
+                            intersections.put(intersectionVariable, new HashSet<>());
+                        intersections.get(intersectionVariable).add((int) intersectionValue);
+                    }
+                    /**
+                     * a: line1, b: line2, c: intersection point.
+                     * b
+                     * a c a
+                     *     b
+                     */
+                    m1 = 0;
+                    m2 = -1;
+                    b1 = variable - (m1 * value);
+                    b2 = anotherVariable - (m2 * assignments.get(anotherVariable));
+                    intersectionValue = (float) (b1 - b2) / (float) (m2 - m1);
+                    if (intersectionValue % 1 == 0 && intersectionValue >= 0 && intersectionValue < variablesCount) {
+                        int intersectionVariable = b2 - m2 * (int) intersectionValue;
+                        if (!intersections.containsKey(intersectionVariable))
+                            intersections.put(intersectionVariable, new HashSet<>());
+                        intersections.get(intersectionVariable).add((int) intersectionValue);
+                    }
+                    /**
+                     * a: line1, b: line2, c: intersection point.
+                     *     b
+                     * a c a
+                     * b
+                     */
+                    m1 = 0;
+                    m2 = 1;
+                    b1 = variable - (m1 * value);
+                    b2 = anotherVariable - (m2 * assignments.get(anotherVariable));
+                    intersectionValue = (float) (b1 - b2) / (float) (m2 - m1);
+                    if (intersectionValue % 1 == 0 && intersectionValue >= 0 && intersectionValue < variablesCount) {
+                        int intersectionVariable = b2 - m2 * (int) intersectionValue;
+                        if (!intersections.containsKey(intersectionVariable))
+                            intersections.put(intersectionVariable, new HashSet<>());
+                        intersections.get(intersectionVariable).add((int) intersectionValue);
+                    }
+                }
+                /**
+                 * find new conflicts that been created because line1 passing this point( variable, value ):
+                 *     1
+                 *   1
+                 * 1
+                 */
+                for (int i = variable - Math.min(value, variable), j = value - Math.min(value, variable); i < variablesCount && j < variablesCount; i++, j++) {
+                    if (!assignments.containsKey(i) && !assignments.containsValue(j) && !(intersections.containsKey(i) && intersections.get(i).contains(j)))
+                        conflicts++;
+                }
+                /**
+                 * find new conflicts that been created because line2 passing this point( variable, value ):
+                 * 2
+                 *   2
+                 *     2
+                 */
+                for (int i = variable + Math.min(variablesCount - variable, value), j = value - Math.min(variablesCount - variable, value); i >= 0 && j < variablesCount; i--, j++) {
+                    if (!assignments.containsKey(i) && !assignments.containsValue(j) && !(intersections.containsKey(i) && intersections.get(i).contains(j)))
+                        conflicts++;
+                }
+                /**
+                 * find new conflicts that been created because line3 passing point( variable, value ):
+                 * 3 3 3
+                 */
+                for (int i = 0, j = value; i < variablesCount; i++) {
+                    if (!assignments.containsKey(i) && !(intersections.containsKey(i) && intersections.get(i).contains(j)))
+                        conflicts++;
+                }
+                candidateValues.add(new CandidateValue(value, conflicts));
+            }
+        }
+        return candidateValues;
+    }
+}
+
+/**
+ * simple class for candidate values of variables.
+ */
+class CandidateValue implements Comparable {
+    int value;
+    int conflicts;
+
+    public CandidateValue(int value, int conflicts) {
+        this.value = value;
+        this.conflicts = conflicts;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        try {
+            CandidateValue cv = (CandidateValue) o;
+            return conflicts - cv.conflicts;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
 }
